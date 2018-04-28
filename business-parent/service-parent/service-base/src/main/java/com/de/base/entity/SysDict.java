86fb868f9aea83e5718cb858eea24808a06842ff
shenjian <shenjian@52cx.com>
2018-04-28 17:25:32 +0800

    msg

---
+++ business-parent/service-parent/service-base/src/main/java/com/de/base/entity/SysDict.java
@@ -0,0 +1,113 @@
+package com.de.base.entity;
+
+public class SysDict {
+    private String id;
+
+    private String type;
+
+    private String dictKey;
+
+    private String dictCode;
+
+    private String dictText;
+
+    private String dictDesc;
+
+    private String dictOrder;
+
+    private String dictPcode;
+
+    private String reserve1;
+
+    private String reserve2;
+
+    private String reserve3;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+
+    public String getDictKey() {
+        return dictKey;
+    }
+
+    public void setDictKey(String dictKey) {
+        this.dictKey = dictKey == null ? null : dictKey.trim();
+    }
+
+    public String getDictCode() {
+        return dictCode;
+    }
+
+    public void setDictCode(String dictCode) {
+        this.dictCode = dictCode == null ? null : dictCode.trim();
+    }
+
+    public String getDictText() {
+        return dictText;
+    }
+
+    public void setDictText(String dictText) {
+        this.dictText = dictText == null ? null : dictText.trim();
+    }
+
+    public String getDictDesc() {
+        return dictDesc;
+    }
+
+    public void setDictDesc(String dictDesc) {
+        this.dictDesc = dictDesc == null ? null : dictDesc.trim();
+    }
+
+    public String getDictOrder() {
+        return dictOrder;
+    }
+
+    public void setDictOrder(String dictOrder) {
+        this.dictOrder = dictOrder == null ? null : dictOrder.trim();
+    }
+
+    public String getDictPcode() {
+        return dictPcode;
+    }
+
+    public void setDictPcode(String dictPcode) {
+        this.dictPcode = dictPcode == null ? null : dictPcode.trim();
+    }
+
+    public String getReserve1() {
+        return reserve1;
+    }
+
+    public void setReserve1(String reserve1) {
+        this.reserve1 = reserve1 == null ? null : reserve1.trim();
+    }
+
+    public String getReserve2() {
+        return reserve2;
+    }
+
+    public void setReserve2(String reserve2) {
+        this.reserve2 = reserve2 == null ? null : reserve2.trim();
+    }
+
+    public String getReserve3() {
+        return reserve3;
+    }
+
+    public void setReserve3(String reserve3) {
+        this.reserve3 = reserve3 == null ? null : reserve3.trim();
+    }
+}
